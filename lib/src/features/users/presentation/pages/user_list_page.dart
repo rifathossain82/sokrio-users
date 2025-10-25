178b375c68aa967b4bc344cde44487c29092fb5f
Rifat Hossain <88751768+rifathossain82@users.noreply.github.com>
2025-10-25 22:43:26 +0600

    chore: readme updated (#5)
    * chore: readme file updated
    
    * chore: go router mocks
    
    * fix: user list page for render issue
    
    * add: download section on readme file
    
    * add: screenshots in readme file
    
    ---------
    
    Co-authored-by: rifathossain82 <appdev.rifathossain@gmail.com>

--- lib/src/features/users/presentation/pages/user_list_page.dart
+++ lib/src/features/users/presentation/pages/user_list_page.dart
@@ -5,7 +5,6 @@ import 'package:sokrio_users/src/core/core.dart';
 import 'package:sokrio_users/src/features/users/presentation/bloc/user_event.dart';
 import 'package:sokrio_users/src/features/users/presentation/bloc/user_state.dart';
 import 'package:sokrio_users/src/features/users/presentation/widgets/user_list_widget.dart';
-import 'package:sokrio_users/src/features/users/presentation/widgets/user_search_field.dart';
 
 class UserListPage extends StatefulWidget {
   const UserListPage({super.key});
@@ -17,13 +16,13 @@ class UserListPage extends StatefulWidget {
 class _UserListPageState extends State<UserListPage> with InfiniteScrollMixin {
   late final UserBloc userBloc;
   late final TextEditingController searchController;
+  bool isSearching = false;
 
   @override
   void initState() {
     super.initState();
     userBloc = BlocProvider.of<UserBloc>(context)..add(const LoadUsersEvent());
     searchController = TextEditingController();
-
     initInfiniteScroll(_loadMore);
   }
 
@@ -41,71 +40,95 @@ class _UserListPageState extends State<UserListPage> with InfiniteScrollMixin {
   }
 
   Future<void> _onRefresh() async {
+    if (isSearching) {
+      setState(() {
+        isSearching = false;
+      });
+      context.unFocusKeyboard();
+      searchController.clear();
+    }
     userBloc.add(const RefreshUsersEvent());
     await userBloc.stream.firstWhere((state) => !state.users.isLoading);
   }
 
-  void onSearch(String query) {
+  void _onSearch(String query) {
     userBloc.add(SearchUsersEvent(query));
   }
 
-  void onClear() {
+  void _closeSearch() {
+    setState(() {
+      isSearching = false;
+    });
     context.unFocusKeyboard();
     searchController.clear();
     userBloc.add(const SearchUsersEvent(''));
   }
 
+  void _openSearch() {
+    setState(() {
+      isSearching = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Users')),
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        elevation: 2,
+        title: isSearching
+            ? TextField(
+                controller: searchController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  hintText: 'Search users...',
+                  border: InputBorder.none,
+                ),
+                onChanged: _onSearch,
+              )
+            : const Text('Users'),
+        actions: [
+          if (isSearching)
+            IconButton(icon: const Icon(Icons.clear), onPressed: _closeSearch)
+          else
+            IconButton(icon: const Icon(Icons.search), onPressed: _openSearch),
+        ],
+      ),
       body: CustomRefreshIndicator(
         onRefresh: _onRefresh,
-        child: Column(
-          children: [
-            UserSearchField(
-              controller: searchController,
-              hintText: 'Search users...',
-              onChanged: onSearch,
-              onClear: onClear,
-            ),
-            Expanded(
-              child: BlocConsumer<UserBloc, UserState>(
-                listenWhen: (previous, current) =>
-                    current.users.shouldShowErrorMessage,
-                listener: (context, state) {
-                  if (state.users.message.isNotEmpty) {
-                    SnackBarService.show(
-                      message: state.users.message,
-                      type: SnackBarType.error,
-                    );
-                  }
+        child: BlocConsumer<UserBloc, UserState>(
+          listenWhen: (previous, current) =>
+              current.users.shouldShowErrorMessage,
+          listener: (context, state) {
+            if (state.users.message.isNotEmpty) {
+              SnackBarService.show(
+                message: state.users.message,
+                type: SnackBarType.error,
+              );
+            }
+          },
+          builder: (context, state) {
+            final usersState = state.users;
+
+            if (usersState.isLoading) {
+              return const KCustomLoader();
+            }
+
+            if (usersState.isEmpty) {
+              return FailureWidgetBuilder(
+                title: 'No users found!',
+                onRetry: () {
+                  userBloc.add(const LoadUsersEvent());
                 },
-                builder: (context, state) {
-                  final usersState = state.users;
+              );
+            }
 
-                  if (usersState.isLoading) {
-                    return const KCustomLoader();
-                  }
-
-                  if (usersState.isEmpty) {
-                    return FailureWidgetBuilder(
-                      title: 'No users found!',
-                      onRetry: () {
-                        userBloc.add(LoadUsersEvent());
-                      },
-                    );
-                  }
-
-                  return UserListWidget(
-                    users: usersState.items,
-                    scrollController: scrollController,
-                    hasMore: usersState.hasMore,
-                  );
-                },
-              ),
-            ),
-          ],
+            return UserListWidget(
+              users: usersState.items,
+              scrollController: scrollController,
+              hasMore: usersState.hasMore,
+            );
+          },
         ),
       ),
     );
